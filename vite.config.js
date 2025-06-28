b77d54c4f36914daf4beffd6b515592283c2d1c1
Kate Walker <katerebeccawalker@gmail.com>
2025-06-27 22:11:13 -0400

    remove explicit env loading

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
